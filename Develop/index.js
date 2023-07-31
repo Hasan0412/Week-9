bae45590bd2360568ad2881faeafb24d92acd4c8
Hasan Malik <hmalik0412@gmail.com>
2023-07-31 14:25:33 +0100

    Added function to write README.md

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,8 @@
-// TODO: Include packages needed for this application
+// Included packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -46,9 +46,17 @@ const questions = [
       },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('README.md successfully created!');
+      }
+    });
+  }
+  
 // TODO: Create a function to initialize app
 function init() {}
 
