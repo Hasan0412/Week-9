28e1aa237fd3f7f6080ae148c48bc8f15bb5d93b
Hasan Malik <hmalik0412@gmail.com>
2023-08-21 14:10:39 +0100

    Correction to integrate generateMarkdown in index

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // Included packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // Array of questions for user input
 const questions = [
